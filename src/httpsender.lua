2af328619a021c48aee5e775f803996c790b229d
Houtje <abyssalwhip@live.nl>
2020-04-10 11:58:15 +0200

    cleanup / post test

--- src/httpsender.lua
+++
@@ -1,8 +0,0 @@
--- to load local lib files:
-package.path = './httplib/?.lua;' .. package.path
-package.cpath = './httplib/?.dll;' .. package.cpath
-
--- test random http query
-socket = require "socket"
-http = require "socket.http"
-print(http.request("https://google.com"))
--- src/script.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$im = imagecreatefrompng("D:\Dev\mp_nuzlocke\sprites\p1.png");
-	header('Content-Type: image/png');
-	imagepng($im);
-	imagedestroy($im);
-?>

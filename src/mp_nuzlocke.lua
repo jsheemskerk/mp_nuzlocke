55cb15fe3fb9a74f3dc5f1d36f40e7b454a70d1e
Houtje <abyssalwhip@live.nl>
2020-05-02 14:43:40 +0200

    new update v1.0

--- src/mp_nuzlocke.lua
+++ src/mp_nuzlocke.lua
@@ -76,7 +76,7 @@ function get_bits(bstr, loc, nbits)
 end
 
 -- Returns the pokemon data at a certain address.
-function get_decrypted_data(address, personality, key)
+function get_decrypted_data(addr, personality, key)
 	local data = {}
 	local data_order = data_orders[(personality % #data_orders) + 1]
 	for i = 1, 4 do
@@ -84,7 +84,7 @@ function get_decrypted_data(address, personality, key)
 		for j = 1, 3 do
 			block[j] = bit.bxor(
 				read_dword(
-					address + offsets.poke["data"] +
+					addr + offsets.poke["data"] +
 					data_order[i] * data_sizes["block"] +
 					(j - 1) * data_sizes["dword"]
 				), key
@@ -93,6 +93,31 @@ function get_decrypted_data(address, personality, key)
 		data[i] = block
 	end
 	return data
+end
+
+-- Returns the EVs stored in the data.
+function get_evs(data)
+	local evs = ''
+	for i = 1, 4 do
+		evs = evs .. get_bits(data[3][1], (i - 1) * 8, 8) .. ','
+	end
+	return evs .. get_bits(data[3][2], 0, 8) .. ',' .. get_bits(data[3][2], 8, 8)
+end
+
+-- Returns the gender, which is based upon a pokemon's personality and pindex.
+function get_gender(personality, pindex)
+	local threshold = read_byte(
+		addresses["base_stats"] +
+		offsets.base_stats["gender"] +
+		(pindex - 1) * data_sizes["base_stats"]
+	)
+	if threshold ~= 0xFF then
+		if threshold == 0xFE then return 2
+		elseif threshold == 0 or get_bits(personality, 0, 8) >= threshold then return 1
+		else return 2
+		end
+	end
+	return 0
 end
 
 -- Returns the current ingame time in seconds.
@@ -104,6 +129,15 @@ function get_ingame_time()
 	return 3600 * get_bits(time_data, 0, 16) +
 		   60 * get_bits(time_data, 16, 8) +
 		   get_bits(time_data, 24, 8)
+end
+
+-- Returns the IVs stored in the data.
+function get_ivs(data)
+	local ivs = get_bits(data[4][2], 0, 5)
+	for i = 1, 5 do
+		ivs = ivs .. ',' .. get_bits(data[4][2], i * 5, 5)
+	end
+	return ivs
 end
 
 -- Returns the current league progression.
@@ -265,6 +299,206 @@ function update_trainer()
 			)
 		end
 	end
+end
+
+-- Updates a pokemon in the current box.
+function update_box(boxes_addr, box_id, slot)
+	local slot_addr = -1
+	if box_id < const["n_boxes"] then
+		slot_addr = boxes_addr + offsets.boxes["pokes"] +
+					box_id * const["box_size"] * data_sizes["boxed_poke"] +
+					(slot - 1) * data_sizes["boxed_poke"]
+	end
+	local personality = read_dword(slot_addr)
+
+	if personality ~= 0 then
+		local banked = "t"
+
+		local tid = read_dword(slot_addr + offsets.poke["tid"])
+		local key = bit.bxor(personality, tid)
+		local data = get_decrypted_data(slot_addr, personality, key)
+
+		local pid = personality
+		local pindex = get_bits(data[1][1], 0, 16)
+		if pindex == const["shedinja_pindex"] then pid = pid + 1 end
+
+		if pokes[pid] == nil then
+			local evs = get_evs(data)
+			local gender = get_gender(personality, pindex)
+			local happiness = get_bits(data[1][3], 8, 8)
+			local ivs = get_ivs(data)
+			local loc_met = as_location(get_bits(data[4][1], 8, 8))
+			local lvl = 0
+			local nature = natures[(personality % 25) + 1]
+			local nick = get_name(slot_addr + offsets.poke["nick"], 10)
+			local time_met = get_ingame_time()
+
+			local poke_data = [[{
+				"banked": "]] .. banked .. [[",
+				"evs": []] .. evs .. [[],
+				"gender": ]] .. gender .. [[,
+				"happiness": ]] .. happiness .. [[,
+				"ivs": []] .. ivs .. [[],
+				"loc_met": "]] .. loc_met .. [[",
+				"lvl": ]] .. lvl .. [[,
+				"nature": []] .. nature .. [[],
+				"nick": "]] .. nick .. [[",
+				"pindex": ]] .. pindex .. [[,
+				"pid": ]] .. pid .. [[,
+				"tid": ]] .. tid .. [[,
+				"time_met": "]] .. time_met .. [["
+			}]]
+
+			post_poke(poke_data, nick)
+
+			local hp = 0
+			local tname = get_name(slot_addr + offsets.poke["tname"], 7)
+
+			pokes[pid] = {
+				["banked"] = banked, ["hp"] = hp, ["lvl"] = lvl, ["nick"] = nick,
+				["pindex"] = pindex, ["tname"] = tname
+			}
+		end
+
+		if pokes[pid]["banked"] ~= banked then
+			http.request(
+				"http://www.joran.fun/nuzlocke/db/updatepokemon.php?pid=" .. pid .. 
+				"&banked=" .. banked
+			)
+			pokes[pid]["banked"] = banked
+		end
+	end
+end
+
+-- Updates a pokemon in the party.
+function update_party(slot)
+	local slot_addr = addresses["party"] + (slot - 1) * data_sizes["poke"]
+	local personality = read_dword(slot_addr)
+
+	if personality ~= 0 then
+		local banked = "f"
+
+		local tid = read_dword(slot_addr + offsets.poke["tid"])
+		local key = bit.bxor(personality, tid)
+		local data = get_decrypted_data(slot_addr, personality, key)
+
+		local pid = personality
+		local pindex = get_bits(data[1][1], 0, 16)
+		if pindex == const["shedinja_pindex"] then pid = pid + 1 end
+
+		local evs = get_evs(data)
+		local happiness = get_bits(data[1][3], 8, 8)
+		local lvl = read_byte(slot_addr + offsets.poke["lvl"])
+		local nick = get_name(slot_addr + offsets.poke["nick"], 10)
+
+		if pokes[pid] == nil then
+			local gender = get_gender(personality, pindex)
+			local ivs = get_ivs(data)
+			local loc_met = as_location(get_bits(data[4][1], 8, 8))
+			local nature = natures[(personality % 25) + 1]
+			local time_met = get_ingame_time()
+
+			local poke_data = [[{
+				"banked": "]] .. banked .. [[",
+				"evs": []] .. evs .. [[],
+				"gender": ]] .. gender .. [[,
+				"happiness": ]] .. happiness .. [[,
+				"ivs": []] .. ivs .. [[],
+				"loc_met": "]] .. loc_met .. [[",
+				"lvl": ]] .. lvl .. [[,
+				"nature": []] .. nature .. [[],
+				"nick": "]] .. nick .. [[",
+				"pindex": ]] .. pindex .. [[,
+				"pid": ]] .. pid .. [[,
+				"tid": ]] .. tid .. [[,
+				"time_met": "]] .. time_met .. [["
+			}]]
+
+			local hp = read_word(slot_addr + offsets.poke["hp"])
+			local tname = get_name(slot_addr + offsets.poke["tname"], 7)
+
+			pokes[pid] = {
+				["banked"] = banked, ["hp"] = hp, ["lvl"] = lvl, ["nick"] = nick,
+				["pindex"] = pindex, ["tname"] = tname
+			}
+
+			if (tid ~= const["steven_tid"] and not (loc_met == "Petalburg City" and
+				pindex == const["zigzagoon_pindex"])) then
+				post_poke(poke_data, nick)
+			end
+		end
+
+		if bit.bxor(pindex, get_bits(key, 0, 16)) ~= pokes[pid].pindex then
+			if hp == 0 and pokes[pid].hp ~= 0 then
+				local opp_addr = addresses["opp_party"]
+				while (read_word(opp_addr + offsets.poke["hp"]) == 0 and
+					   read_dword(opp_addr) ~= 0) do
+					opp_addr = opp_addr + data_sizes["poke"]
+				end
+				local opp_personality = read_dword(opp_addr)
+				local opp_tid = read_dword(opp_addr + offsets.poke["tid"])
+				local opp_data = get_decrypted_data(opp_addr, opp_personality, opp_tid)
+				local opp_pindex = get_bits(opp_data[1][1], 0, 16)
+				local loc_died, _ = string.gsub(
+					as_location(get_bits(opp_data[4][1], 8, 8)), " ", "%%20"
+				)
+				http.request(
+					"http://www.joran.fun/nuzlocke/db/updatepokemon.php?pid=" .. pid ..
+					"&loc_died=" .. loc_died .. "&time_died=" .. time .. "&opp=" ..
+					opp_pindex .. "&died"
+				)
+			elseif pindex ~= pokes[pid].pindex then
+				http.request(
+					"http://www.joran.fun/nuzlocke/db/updatepokemon.php?pid=" .. pid ..
+					"&lvl=" .. lvl .. "&evs=" .. evs .. "&happiness=" .. happiness ..
+					"&pindex=" .. pindex .. "&nick=" .. nick .. "&tname=" ..
+					string.gsub(tname, " ", "%%20") .. "&evolved"
+				)
+			elseif lvl ~= pokes[pid].lvl then
+				http.request(
+					"http://www.joran.fun/nuzlocke/db/updatepokemon.php?pid=" .. pid ..
+					"&lvl=" .. lvl .. "&evs=" .. evs .. "&happiness=" .. happiness
+				)
+			elseif nick ~= pokes[pid].nick then
+				http.request(
+					"http://www.joran.fun/nuzlocke/db/updatepokemon.php?pid=" .. pid ..
+					"&nick=" .. string.gsub(nick, " ", "%%20") .. "&tname=" ..
+					string.gsub(pokes[pid].tname, " ", "%%20") .. "&pindex=" ..
+					pindex .. "&rename"
+				)
+			end
+			pokes[pid]["pindex"] = pindex
+			pokes[pid]["hp"] = hp
+			pokes[pid]["lvl"] = lvl
+			pokes[pid]["nick"] = nick
+		end
+
+		if pokes[pid]["banked"] ~= banked then
+			http.request(
+				"http://www.joran.fun/nuzlocke/db/updatepokemon.php?pid=" .. pid ..
+				"&banked=" .. banked
+			)
+			pokes[pid]["banked"] = banked
+		end
+	end
+end
+
+-- The script's main function, which updates the data if required.
+function update_new()
+	if frames % const["fps"] == 0 then
+		update_trainer()
+
+		for slot = 1, const["party_size"] do
+			update_party(slot)
+		end
+
+		local boxes_addr = addresses["boxes_base"] + read_byte(addresses["save_offset_byte"])
+		local box_id = read_dword(boxes_addr)
+		for slot = 1, const["box_size"] do
+			update_box(boxes_addr, box_id, slot)
+		end
+	end
+	frames = frames + 1
 end
 
 -- The script's main function, which updates the data if required.
@@ -432,4 +666,4 @@ function update()
 end
 
 -- Applies the main function on a per-frame basis.
-gui.register(update)
+gui.register(update_new)
